4aca8e8f3c052f47a2414481179293fcd7771736
Sakhman <68939201+Sakhman@users.noreply.github.com>
2021-05-03 10:15:51 +0530

    Update payout.py

--- cogs/payout.py
+++ cogs/payout.py
@@ -126,7 +126,7 @@ class Cashout(commands.Cog):
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             await ctx.send(embed=embed)
 
-    @cashout.error
+    cashout.error
     async def on_command_error(self, ctx, error):
         if isinstance(error, Exception):
             print(error)
