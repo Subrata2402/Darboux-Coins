e858c66fd65bbdf7fe4317a4f915be65d8151b42
Sakhman <68939201+Sakhman@users.noreply.github.com>
2021-05-04 19:42:31 +0530

    Update glogin.py

--- cogs/glogin.py
+++ cogs/glogin.py
@@ -36,7 +36,10 @@ class Google(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["glinkverify","googleverify"])
-    async def gverify(self, ctx, url):
+    async def gverify(self, ctx, url=None):
+        if url is None:
+            embed=discord.Embed(title="⚠️ Invalid Argument", description=f"Use `{ctx.prefix} <url>` to add an HQ Trivia account in bot.", color=0x00ffff)
+            return await ctx.send(embed=embed)
         embed=discord.Embed(title="Verification Disabled", description="Sorry, this process is not available right now. Please try again later.", color=0x00ffff)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/823971641959776326/834684689059020820/1200px-Google__G__Logo.svg.png")
         await ctx.send(embed=embed)
