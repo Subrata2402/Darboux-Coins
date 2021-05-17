3de37c39a3faae8523d3e8a6b9e04ca88b8d5a06
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-05-17 14:34:15 +0530

    Update sdcplay.py

--- cogs/sdcplay.py
+++ cogs/sdcplay.py
@@ -43,7 +43,7 @@ class DcPlay(commands.Cog):
         for i in all_data:
             name_list.append(i['username'])
         if username not in name_list:
-            embed=discord.Embed(title="❎ Not Found", description=f"No account found with name `{username}`. Use Command `{ctx.prefix}accounts` to check your all accounts.", color=0x00ffff)
+            embed=discord.Embed(title="❎ Not Found", description=f"No account found with name `{username}`. Use Command `{ctx.prefix}accounts` to check your all accounts.", color=discord.Colour.random())
             embed.set_thumbnail(url=self.client.user.avatar_url)
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             return await ctx.send(embed=embed)
@@ -52,15 +52,16 @@ class DcPlay(commands.Cog):
             api = HQApi(token)
             data = api.get_users_me()
         except ApiResponseError:
-            embed=discord.Embed(title="⚠️ Token Expired", description=f"This account token is expired. Please use Command `{ctx.prefix}refresh {username}` to refresh your account.", color=0x00ffff)
+            embed=discord.Embed(title="⚠️ Token Expired", description=f"This account token is expired. Please use Command `{ctx.prefix}refresh {username}` to refresh your account.", color=discord.Colour.random())
             embed.set_thumbnail(url=self.client.user.avatar_url)
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             return await ctx.send(embed=embed)
         coins = data["coins"]
-        embed=discord.Embed(title="Playing HQ Offair Trivia...", color=0x00ffff)
+        embed=discord.Embed(title="Starting HQ Offair Trivia...", color=discord.Colour.random())
         x = await ctx.send(embed=embed)
         username = "||Private Account||"
-        embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 00\n• Questions Correct : 00/00\n• Coins Earned : 0\n• Total Coins : {coins}**", color=0x00ffff)
+        await asyncio.sleep(2)
+        embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 00\n• Questions Correct : 00/00\n• Coins Earned : 0\n• Total Coins : {coins}**", color=discord.Colour.random())
         embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
         embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
         await x.edit(embed=embed)
@@ -82,10 +83,10 @@ class DcPlay(commands.Cog):
             seconds=int(seconds)
             await asyncio.sleep(1)
             if hours <= 0:
-                embed=discord.Embed(description=f"You have played all games as of now, so you must wait **{minutes}** minute(s) **{seconds}** second(s) to play Daily Challenge once again.", color=0x00ffff)
+                embed=discord.Embed(description=f"You have played all games as of now, so you must wait **{minutes}** minute(s) **{seconds}** second(s) to play Daily Challenge once again.", color=discord.Colour.random())
                 await x.edit(embed=embed)
             else:
-                embed=discord.Embed(description=f"You have played all games as of now, so you must wait **{hours}** hour(s) **{minutes}** minute(s) and **{seconds}** second(s) to play Daily Challenge once again.", color=0x00ffff)
+                embed=discord.Embed(description=f"You have played all games as of now, so you must wait **{hours}** hour(s) **{minutes}** minute(s) and **{seconds}** second(s) to play Daily Challenge once again.", color=discord.Colour.random())
                 await x.edit(embed=embed)
             offair_id = api.get_schedule()['offairTrivia']['games'][0]['gameUuid']
         while True:
@@ -147,7 +148,7 @@ class DcPlay(commands.Cog):
                 tcoins = str(data['gameSummary']['coinsTotal'])
                 coins = str(data['gameSummary']['coinsEarned'])
                 correct = str(data['gameSummary']['questionsCorrect'])
-                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct}/12\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=0x00ffff)
+                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct}/12\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
                 await x.edit(embed=embed)
@@ -157,7 +158,7 @@ class DcPlay(commands.Cog):
         try:
             offair_id = api.start_offair()['gameUuid']
         except ApiResponseError:
-            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct1}/12\n• Coins Earned : {coins1}\n• Total Coins : {tcoins}**", color=0x00ffff)
+            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct1}/12\n• Coins Earned : {coins1}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             embed.set_thumbnail(url=self.client.user.avatar_url)
             await x.edit(embed=embed)
@@ -223,7 +224,7 @@ class DcPlay(commands.Cog):
                 correct = str(data['gameSummary']['questionsCorrect'])
                 cn = int(coins1) + int(coins)
                 ct = int(correct1) + int(correct)
-                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 02\n• Questions Correct : {ct}/24\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=0x00ffff)
+                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 02\n• Questions Correct : {ct}/24\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
                 await x.edit(embed=embed)
@@ -235,7 +236,7 @@ class DcPlay(commands.Cog):
         except ApiResponseError:
             coins = int(coins1) + int(coins2)
             correct = int(correct1) + int(correct2)
-            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 02\n• Questions Correct : {correct}/24\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=0x00ffff)
+            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 02\n• Questions Correct : {correct}/24\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             embed.set_thumbnail(url=self.client.user.avatar_url)
             await x.edit(embed=embed)
@@ -301,7 +302,7 @@ class DcPlay(commands.Cog):
                 correct = str(data['gameSummary']['questionsCorrect'])
                 cn = int(coins1) + int(coins2) + int(coins)
                 ct = int(correct1) + int(correct2) + int(correct)
-                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 03\n• Questions Correct : {ct}/36\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=0x00ffff)
+                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 03\n• Questions Correct : {ct}/36\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
                 await x.edit(embed=embed)
@@ -313,7 +314,7 @@ class DcPlay(commands.Cog):
         except ApiResponseError:
             coins = int(coins1) + int(coins2) + int(coins3)
             correct = int(correct1) + int(correct2) + int(correct3)
-            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 03\n• Questions Correct : {correct}/36\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=0x00ffff)
+            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 03\n• Questions Correct : {correct}/36\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             embed.set_thumbnail(url=self.client.user.avatar_url)
             await x.edit(embed=embed)
@@ -379,7 +380,7 @@ class DcPlay(commands.Cog):
                 correct = str(data['gameSummary']['questionsCorrect'])
                 cn = int(coins1) + int(coins2) + int(coins) + int(coins3)
                 ct = int(correct1) + int(correct2) + int(correct) + int(correct3)
-                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 04\n• Questions Correct : {ct}/48\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=0x00ffff)
+                embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 04\n• Questions Correct : {ct}/48\n• Coins Earned : {cn}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
                 await x.edit(embed=embed)
@@ -391,7 +392,7 @@ class DcPlay(commands.Cog):
         except ApiResponseError:
             coins = int(coins1) + int(coins2) + int(coins3) + int(coins4)
             correct = int(correct1) + int(correct2) + int(correct3) + int(correct4)
-            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 04\n• Questions Correct : {correct}/48\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=0x00ffff)
+            embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 04\n• Questions Correct : {correct}/48\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             embed.set_thumbnail(url=self.client.user.avatar_url)
             await x.edit(embed=embed)
@@ -457,7 +458,7 @@ class DcPlay(commands.Cog):
                 correct = str(data['gameSummary']['questionsCorrect'])
                 coins = int(coins) + int(coins1) + int(coins2) + int(coins3) + int(coins)
                 correct = int(correct) + int(correct1) + int(correct2) + int(correct3) + int(correct)
-                embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 05\n• Questions Correct : {correct}/60\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=0x00ffff)
+                embed=discord.Embed(title="**Played Offair Trivia ✅**", description=f"**• Username : {username}\n• Games Played : 05\n• Questions Correct : {correct}/60\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.set_thumbnail(url=self.client.user.avatar_url)
                 await x.edit(embed=embed)
