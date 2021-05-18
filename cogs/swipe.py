7d6ec5829e6d09ba54a91f994a58ac4e08b7bb52
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-05-18 14:12:59 +0530

    Create swipe.py

---
+++ cogs/swipe.py
@@ -0,0 +1,70 @@
+import discord
+import random
+from discord.ext import commands
+import asyncio
+from pymongo import MongoClient
+from HQApi import HQApi
+from HQApi.exceptions import ApiResponseError
+from HQApi import HQApi, HQWebSocket
+import asyncio
+from datetime import datetime
+import requests
+import json
+import time
+import colorsys
+import datetime
+import aniso8601
+from pytz import timezone
+from unidecode import unidecode
+from bs4 import BeautifulSoup
+
+data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/Darboux?retryWrites=true&w=majority')#Your Database Url
+db = data.get_database("Darboux")#Your db name
+token_base = db.token
+login_token_base = db.login_token
+
+
+class Swipe(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def swipe(self, ctx, username=None):
+        """Swipe and earn extra life."""
+        if username is None:
+            embed=discord.Embed(title="⚠️ Invalid Command", description=f"Use `{ctx.prefix}swipe [username]` to swipe your account and earn an Extra Life.", color=discord.Colour.random())
+            return await ctx.send(embed=embed)
+        commander_id = ctx.author.id
+        name_list = []
+        all_data = list(token_base.find({"id": commander_id, "username": username}))
+        for i in all_data:
+            name_list.append(i['username'])
+        if username not in name_list:
+            embed=discord.Embed(title="❎ Not Found", description=f"No account found with name `{username}`. Use Command `{ctx.prefix}accounts` to check your all accounts.", color=discord.Colour.random())
+            embed.set_thumbnail(url=self.client.user.avatar_url)
+            embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
+            return await ctx.send(embed=embed)
+        token = token_base.find_one({'username': username})['token']
+        try:
+            api = HQApi(token)
+            data = api.get_users_me()
+        except ApiResponseError:
+            embed=discord.Embed(title="⚠️ Token Expired", description=f"Your account token is expired. Please refresh your account by this command.\n`{ctx.prefix}refresh {username}`", color=discord.Colour.random())
+            embed.set_thumbnail(url=self.client.user.avatar_url)
+            embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
+            return await ctx.send(embed=embed)
+        headers = {"Authorization": f"Bearer {token}"}
+        try:
+            r = requests.post("https://api-quiz.hype.space/easter-eggs/makeItRain", headers=headers)
+            embed=discord.Embed(title="Swiped Done ✅", description=f"You have successfully swiped your account and earn an Extra Life.", color=discord.Colour.random())
+            embed.set_thumbnail(url=self.client.user.avatar_url)
+            embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
+            await ctx.send(embed=embed)
+        except:
+            embed=discord.Embed(description=f"You have already swiped your account.", color=discord.Colour.random())
+            return await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Swipe(client))
