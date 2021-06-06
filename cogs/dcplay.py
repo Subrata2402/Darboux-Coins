ad30c2d71157a0f24fde7b6970572e3403938d16
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-06-06 14:12:29 +0530

    Update dcplay.py

--- cogs/dcplay.py
+++ cogs/dcplay.py
@@ -124,10 +124,15 @@ class DcPlay(commands.Cog):
             option3=f"{answers[2]}"
 
             if question in q_list:
-                option = q_base.find_one({'question': question})['option']
-                select = int(option)
+                answer = q_base.find_one({'question': question})['answer']
+                if option1 == answer:
+                    select = 1
+                elif option2 == answer:
+                    select = 2
+                else:
+                    select = 3
             else:
-                select = int(2)
+                select = 2
             data = api.send_offair_answer(offair_id, offair['question']['answers'][select - 1]['offairAnswerId'])
             #print(data)
             answer_counts = {}
@@ -197,10 +202,15 @@ class DcPlay(commands.Cog):
             option3=f"{answers[2]}"
             
             if question in q_list:
-                option = q_base.find_one({'question': question})['option']
-                select = int(option)
+                answer = q_base.find_one({'question': question})['answer']
+                if option1 == answer:
+                    select = 1
+                elif option2 == answer:
+                    select = 2
+                else:
+                    select = 3
             else:
-                select = int(2)
+                select = 2
             data = api.send_offair_answer(offair_id, offair['question']['answers'][select - 1]['offairAnswerId'])
             #print(data)
             answer_counts = {}
@@ -275,10 +285,15 @@ class DcPlay(commands.Cog):
             option3=f"{answers[2]}"
             
             if question in q_list:
-                option = q_base.find_one({'question': question})['option']
-                select = int(option)
+                answer = q_base.find_one({'question': question})['answer']
+                if option1 == answer:
+                    select = 1
+                elif option2 == answer:
+                    select = 2
+                else:
+                    select = 3
             else:
-                select = int(2)
+                select = 2
             data = api.send_offair_answer(offair_id, offair['question']['answers'][select - 1]['offairAnswerId'])
             #print(data)
             answer_counts = {}
@@ -353,10 +368,15 @@ class DcPlay(commands.Cog):
             option3=f"{answers[2]}"
             
             if question in q_list:
-                option = q_base.find_one({'question': question})['option']
-                select = int(option)
+                answer = q_base.find_one({'question': question})['answer']
+                if option1 == answer:
+                    select = 1
+                elif option2 == answer:
+                    select = 2
+                else:
+                    select = 3
             else:
-                select = int(2)
+                select = 2
             data = api.send_offair_answer(offair_id, offair['question']['answers'][select - 1]['offairAnswerId'])
             #print(data)
             answer_counts = {}
@@ -431,10 +451,15 @@ class DcPlay(commands.Cog):
             option3=f"{answers[2]}"
             
             if question in q_list:
-                option = q_base.find_one({'question': question})['option']
-                select = int(option)
+                answer = q_base.find_one({'question': question})['answer']
+                if option1 == answer:
+                    select = 1
+                elif option2 == answer:
+                    select = 2
+                else:
+                    select = 3
             else:
-                select = int(2)
+                select = 2
             data = api.send_offair_answer(offair_id, offair['question']['answers'][select - 1]['offairAnswerId'])
             #print(data)
             answer_counts = {}
