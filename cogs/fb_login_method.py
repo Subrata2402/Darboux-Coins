2fdf031b6245bc752d510459d88b209b8a5f83d6
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-06-18 21:28:17 +0530

    Update fb_login_method.py

--- cogs/fb_login_method.py
+++ cogs/fb_login_method.py
@@ -24,7 +24,7 @@ db = data.get_database("Darboux")#Your db name
 token_base = db.token
 q_base = db.questions
 
-class DcPlay(commands.Cog):
+class FbMethod(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -64,20 +64,54 @@ class DcPlay(commands.Cog):
         embed7.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
         embed7.timestamp = datetime.datetime.utcnow()
 
-        embed8=discord.Embed(title="**____**", description=f"**Then use `{ctx.prefix}fblogin [fbtoken]` and successfully add your account in bot database. Use `{ctx.prefix}accounts` to check your saved accounts.**", color=0x00ffff)
+        embed8=discord.Embed(title="**__Last Step__**", description=f"**Then use `{ctx.prefix}fblogin [fbtoken]` and successfully add your account in bot database. Use `{ctx.prefix}accounts` to check your saved accounts.**", color=0x00ffff)
         embed8.set_image(url="https://cdn.discordapp.com/attachments/838633900950552606/843353243500150794/IMG_20210516_103423.jpg")
         embed8.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
         embed8.timestamp = datetime.datetime.utcnow()
 
-        paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx)
-        paginator.add_reaction('⏮', "first")
-        paginator.add_reaction('◀', "back")
-        paginator.add_reaction('<:emoji_60:855472859034943488>', "lock")
-        paginator.add_reaction('▶', "next")
-        paginator.add_reaction('⏭', "last")
-        embeds = [embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8]
-        await paginator.run(embeds)
+        pages = [page1, page2, page3, page4, page5]
+
+        message = await ctx.send(embed = page1)
+        await message.add_reaction('⏮')
+        await message.add_reaction('◀')
+        await message.add_reaction('▶')
+        await message.add_reaction('⏭')
+
+        def check(reaction, user):
+            return user == ctx.author
+
+        i = 0
+        reaction = None
+
+        while True:
+            if str(reaction) == '⏮':
+                i = 0
+                await message.edit(embed = pages[i])
+            elif str(reaction) == '◀':
+                if i > 0:
+                    i -= 1
+                    await message.edit(embed = pages[i])
+            elif str(reaction) == '▶':
+                if i < 4:
+                    i += 1
+                    await message.edit(embed = pages[i])
+            elif str(reaction) == '⏭':
+                i = 4
+                await message.edit(embed = pages[i])
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', timeout = 60.0, check = check)
+                try:
+                    await message.remove_reaction(reaction, user)
+                except:
+                    pass
+            except:
+                break
+        try:
+            await message.clear_reactions()
+        except:
+            print("Don't have permission to remove reactions.")
+
 
 
 def setup(client):
-    client.add_cog(DcPlay(client))
+    client.add_cog(FbMethod(client))
