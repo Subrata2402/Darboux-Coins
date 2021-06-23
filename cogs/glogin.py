15b1253571418b5fc684892e1567bac8c71cf950
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-06-23 18:40:12 +0530

    Update glogin.py

--- cogs/glogin.py
+++ cogs/glogin.py
@@ -32,21 +32,28 @@ class Google(commands.Cog):
     @commands.command(aliases=["googlelink"])
     @commands.dm_only()
     async def glink(self, ctx):
-        embed=discord.Embed(title="**HQ Google Login**", color=discord.Colour.random())
-        embed.add_field(name="Login Link", value="[Click Here](https://accounts.google.com/o/oauth2/v2/auth?audience=668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com&client_id=668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=https://localhost:8000&verifier=56778634)")
+        if ctx.guild:
+            try:
+                await ctx.message.delete()
+            except:
+                pass
+            await ctx.send(f"{ctx.author.mention}, **Check your DM!**")
+        embed=discord.Embed(title="**HQ Google Login**", description=f"**[Click Here](https://accounts.google.com/o/oauth2/v2/auth?audience=668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com&client_id=668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=https://localhost:8000&verifier=56778634) to login HQ Trivia with Google Account.\n\nUse `{ctx.prefix}gmethod` to get all process of Google Login.**", color=discord.Colour.random())
+        #embed.add_field(name="Login Link", value="[Click Here](87-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=https://localhost:8000&verifier=56778634)")
         embed.set_thumbnail(url=self.client.user.avatar_url)
         embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-        await ctx.send(embed=embed)
-
-    @glink.error
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, Exception):
-            await ctx.send(f"{ctx.author.mention}, **You can use this command only in DM!**")
+        await ctx.author.send(embed=embed)
 
 
     @commands.command(aliases=["glinkverify","googleverify","glogin","hqgverify"])
     @commands.dm_only()
     async def gverify(self, ctx, url=None):
+        if ctx.guild:
+            try:
+                await ctx.message.delete()
+            except:
+                pass
+            return await ctx.send(f"{ctx.author.mention}, **You can use this command only in DM!**")
         if url is None:
             embed=discord.Embed(title="⚠️ Invalid Argument", description=f"Use `{ctx.prefix}glogin <url>` to add an HQ Trivia account in bot.", color=discord.Colour.random())
             return await ctx.send(embed=embed)
@@ -54,11 +61,6 @@ class Google(commands.Cog):
         embed.set_thumbnail(url=self.client.user.avatar_url)
         embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
         await ctx.send(embed=embed)
-
-    @gverify.error
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, Exception):
-            await ctx.send(f"{ctx.author.mention}, **You can use this command only in DM!**")
 
 
 
