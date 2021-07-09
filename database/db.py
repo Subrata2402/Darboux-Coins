14fae613576b68c80058a7e434a020bc11210b01
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-07-09 14:19:41 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,8 @@
+from pymongo import MongoClient
 
+
+data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/Darboux?retryWrites=true&w=majority')
+db = data.get_database("Darboux")
+token_base = db.token
+login_token_base = db.login_token
+q_base = db.questions
