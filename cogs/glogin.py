6d5170544e4d11c90155e297cefdbc6c987e8956
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-24 19:16:57 +0530

    update glogin.py

--- cogs/glogin.py
+++ cogs/glogin.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext import commands
 from HQApi import HQApi
 import requests, json
-from database.db import token_base, login_token_base
+from database import db
 import urllib
 from urllib.parse import unquote
 
 
-class Google(commands.Cog):
+class Google(commands.Cog, HQApi):
 
     def __init__(self, client):
         self.client = client
@@ -49,10 +49,6 @@ class Google(commands.Cog):
     @commands.command(aliases=["glinkverify","googleverify","glogin","hqgverify", "google"])
     async def gverify(self, ctx, url=None):
         if ctx.guild:
-            try:
-                await ctx.message.delete()
-            except:
-                pass
             return await ctx.send(f"{ctx.author.mention}, **You can use this command only in DM!**")
         if url is None:
             embed=discord.Embed(title="**HQ Google Login**", description=f"**[Click Here](https://accounts.google.com/o/oauth2/v2/auth?audience=668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com&client_id=668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=http://localhost:8080&verifier=56778634) to login HQ Trivia with Google Account.\n\nUse `{ctx.prefix}gmethod` to get all process of Google Login.**", color=discord.Colour.random())
@@ -63,10 +59,10 @@ class Google(commands.Cog):
         user_id = ctx.author.id
         channel = self.client.get_channel(841489971109560321)
         id_token = await self.get_id_token(url)
-        api = HQApi()
         if not id_token:
             return await ctx.send(ctx.author.mention + ", **Invalid URL provided!**")
-        data = api.google_login(id_token)
+        data = await self.google_login(id_token)
+        return await ctx.send(data)
         id = data["userId"]
         username = data["username"]
         login_token = data["loginToken"]
