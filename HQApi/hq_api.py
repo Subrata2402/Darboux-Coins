b371f5604906da92aea841ea5d20f2e742fb1bff
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-24 16:38:20 +0530

    update hq_api.py

--- HQApi/hq_api.py
+++ HQApi/hq_api.py
@@ -1,148 +1,144 @@
-import json
-
-import jwt
-import requests
+import json, jwt, requests
 from HQApi.exceptions import ApiResponseError, BannedIPError
-
 
 class BaseHQApi:
     def __init__(self, token: str = None, logintoken: str = None):
         self.token = token
         self.logintoken = logintoken
 
-    def api(self):
+    async def api(self):
         return self
 
-    def get_users_me(self):
-        return self.fetch("GET", "users/me")
+    async def get_users_me(self):
+        return await self.fetch("GET", "users/me")
 
-    def get_user(self, id: str):
-        return self.fetch("GET", "users/{}".format(id))
+    async def get_user(self, id: str):
+        return await self.fetch("GET", "users/{}".format(id))
 
-    def search(self, name):
-        return self.fetch("GET", 'users?q={}'.format(name))
+    async def search(self, name):
+        return await self.fetch("GET", 'users?q={}'.format(name))
 
-    def get_payouts_me(self):
-        return self.fetch("GET", "users/me/payouts")
+    async def get_payouts_me(self):
+        return await self.fetch("GET", "users/me/payouts")
 
-    def get_show(self):
-        return self.fetch("GET", "shows/now")
+    async def get_show(self):
+        return await self.fetch("GET", "shows/now")
 
-    def get_schedule(self):
-        return self.fetch("GET", 'shows/schedule')
+    async def get_schedule(self):
+        return await self.fetch("GET", 'shows/schedule')
 
-    def easter_egg(self, type: str = "makeItRain"):
-        return self.fetch("POST", "easter-eggs/{}".format(type))
+    async def easter_egg(self, type: str = "makeItRain"):
+        return await self.fetch("POST", "easter-eggs/{}".format(type))
 
-    def make_payout(self, email: str):
-        return self.fetch("POST", "users/me/payouts", {"email": email})
+    async def make_payout(self, email: str):
+        return await self.fetch("POST", "users/me/payouts", {"email": email})
 
-    def send_code(self, phone: str, method: str = "sms"):
-        return self.fetch("POST", "verifications", {"phone": phone, "method": method})
+    async def send_code(self, phone: str, method: str = "sms"):
+        return await self.fetch("POST", "verifications", {"phone": phone, "method": method})
 
-    def confirm_code(self, verificationid: str, code: int):
-        return self.fetch("POST", "verifications/{}".format(verificationid), {"code": code})
+    async def confirm_code(self, verificationid: str, code: int):
+        return await self.fetch("POST", "verifications/{}".format(verificationid), {"code": code})
 
-    def register(self, verificationid: str, name: str, referral: str = None):
-        return self.fetch("POST", "users", {
+    async def register(self, verificationid: str, name: str, referral: str = None):
+        return await self.fetch("POST", "users", {
             "country": "MQ==", "language": "eu",
             "referringUsername": referral,
             "username": name,
             "verificationId": verificationid})
 
-    def delete_avatar(self):
-        return self.fetch("DELETE", "users/me/avatarUrl")
+    async def delete_avatar(self):
+        return await self.fetch("DELETE", "users/me/avatarUrl")
 
-    def add_referral(self, referral: str):
-        return self.fetch("PATCH", "users/me", {"referringUsername": referral})
+    async def add_referral(self, referral: str):
+        return await self.fetch("PATCH", "users/me", {"referringUsername": referral})
 
-    def add_friend(self, id: str):
-        return self.fetch("POST", "friends/{}/requests".format(id))
+    async def add_friend(self, id: str):
+        return await self.fetch("POST", "friends/{}/requests".format(id))
 
-    def friend_status(self, id: str):
-        return self.fetch("GET", "friends/{}/status".format(id))
+    async def friend_status(self, id: str):
+        return await self.fetch("GET", "friends/{}/status".format(id))
 
-    def remove_friend(self, id: str):
-        return self.fetch("DELETE", "friends/{}".format(id))
+    async def remove_friend(self, id: str):
+        return await self.fetch("DELETE", "friends/{}".format(id))
 
-    def accept_friend(self, id: str):
-        return self.fetch("PUT", "friends/{}/status".format(id), {"status": "ACCEPTED"})
+    async def accept_friend(self, id: str):
+        return await self.fetch("PUT", "friends/{}/status".format(id), {"status": "ACCEPTED"})
 
-    def friend_list(self):
-        return self.fetch("GET", "friends")
+    async def friend_list(self):
+        return await self.fetch("GET", "friends")
 
-    def check_username(self, name: str):
-        return self.fetch("POST", "usernames/available", {"username": name})
+    async def check_username(self, name: str):
+        return await self.fetch("POST", "usernames/available", {"username": name})
 
-    def get_tokens(self, login_token: str):
-        return self.fetch("POST", "tokens", {'token': login_token})
+    async def get_tokens(self, login_token: str):
+        return await self.fetch("POST", "tokens", {'token': login_token})
 
-    def edit_username(self, username: str):
-        return self.fetch("PATCH", "users/me", {"username": username})
+    async def edit_username(self, username: str):
+        return await self.fetch("PATCH", "users/me", {"username": username})
 
-    def get_login_token(self):
-        return self.fetch("GET", "users/me/token")
+    async def get_login_token(self):
+        return await self.fetch("GET", "users/me/token")
 
-    def send_documents(self, id, email, paypal_email, country):
-        return self.fetch("POST", "users/{}/payouts/documents".format(id),
+    async def send_documents(self, id, email, paypal_email, country):
+        return await self.fetch("POST", "users/{}/payouts/documents".format(id),
                           {"email": email, "country": country, "payout": paypal_email})
 
-    def register_device_token(self, token):
-        return self.fetch("POST", "users/me/devices", {"token": token})
+    async def register_device_token(self, token):
+        return await self.fetch("POST", "users/me/devices", {"token": token})
 
-    def config(self):
-        return self.fetch("GET", "config")
+    async def config(self):
+        return await self.fetch("GET", "config")
 
-    def get_optins(self):
-        return self.fetch("GET", "opt-in")
+    async def get_optins(self):
+        return await self.fetch("GET", "opt-in")
 
-    def set_optin(self, name: str, value: bool):
-        return self.fetch("POST", "opt-in", {"value": value, "opt": name})
+    async def set_optin(self, name: str, value: bool):
+        return await self.fetch("POST", "opt-in", {"value": value, "opt": name})
 
-    def season_xp(self):
-        return self.fetch("GET", "seasonXp/settings")
+    async def season_xp(self):
+        return await self.fetch("GET", "seasonXp/settings")
 
-    def referrals(self):
-        return self.fetch("GET", "show-referrals")
+    async def referrals(self):
+        return await self.fetch("GET", "show-referrals")
 
-    def facebook_login(self, access_token):
-        return self.fetch("POST", "users/provider-auth", {"type":"FACEBOOK","token": access_token})
+    async def facebook_login(self, access_token):
+        return await self.fetch("POST", "users/provider-auth", {"type":"FACEBOOK","token": access_token})
 
-    def google_login(self, jwt_token):
-        return self.fetch("POST", "users/provider-auth", {"type":"GOOGLE","token": jwt_token})
+    async def google_login(self, jwt_token):
+        return await self.fetch("POST", "users/provider-auth", {"type":"GOOGLE","token": jwt_token})
 
-    def swipe(self):
-        return self.fetch("POST", "easter-eggs/makeItRain")
+    async def swipe(self):
+        return await self.fetch("POST", "easter-eggs/makeItRain")
 
-    def purchase_life(self, amount: int):
-        return self.fetch("POST", "store/com.intermedia.hq.item.extralife.{}x/purchase".format(amount))
+    async def purchase_life(self, amount: int):
+        return await self.fetch("POST", "store/com.intermedia.hq.item.extralife.{}x/purchase".format(amount))
 
-    def purchase_eraser(self, amount: int):
-        return self.fetch("POST", "store/com.intermedia.hq.item.erasers.{}x/purchase".format(amount))
+    async def purchase_eraser(self, amount: int):
+        return await self.fetch("POST", "store/com.intermedia.hq.item.erasers.{}x/purchase".format(amount))
 
-    def purchase_super_spin(self, amount: int):
-        return self.fetch("POST", "store/com.intermedia.hq.item.superspin.{}x/purchase".format(amount))
+    async def purchase_super_spin(self, amount: int):
+        return await self.fetch("POST", "store/com.intermedia.hq.item.superspin.{}x/purchase".format(amount))
 
-    def leaderboard(self, mode: str):
-        return self.fetch("GET", "users/leaderboard?mode={}".format(mode))
+    async def leaderboard(self, mode: str):
+        return await self.fetch("GET", "users/leaderboard?mode={}".format(mode))
 
-    def set_avatar(self, file: str):
-        return self.fetch("POST", "users/me/avatar", files={"file": ("file", open(file, 'rb').read(), 'image/jpeg')})
+    async def set_avatar(self, file: str):
+        return await self.fetch("POST", "users/me/avatar", files={"file": ("file", open(file, 'rb').read(), 'image/jpeg')})
 
-    def store(self):
-        return self.fetch("GET", "store/products")
+    async def store(self):
+        return await self.fetch("GET", "store/products")
 
-    def start_offair(self):
-        return self.fetch('POST', "offair-trivia/start-game")
+    async def start_offair(self):
+        return await self.fetch('POST', "offair-trivia/start-game")
 
-    def offair_trivia(self, id: str):
-        return self.fetch('GET', 'offair-trivia/{}'.format(id))
+    async def offair_trivia(self, id: str):
+        return await self.fetch('GET', 'offair-trivia/{}'.format(id))
 
-    def send_offair_answer(self, id: str, answer_id: str):
-        return self.fetch('POST', 'offair-trivia/{}/answers'.format(id), {"offairAnswerId": answer_id})
+    async def send_offair_answer(self, id: str, answer_id: str):
+        return await self.fetch('POST', 'offair-trivia/{}/answers'.format(id), {"offairAnswerId": answer_id})
 
-    def custom(self, method, func, data):
-        return self.fetch(method, func, data)
+    async def custom(self, method, func, data):
+        return await self.fetch(method, func, data)
 
 
 class HQApi(BaseHQApi):
@@ -165,7 +161,7 @@ class HQApi(BaseHQApi):
         if self.token:
             self.headers["Authorization"] = "Bearer " + self.token
 
-    def fetch(self, method="GET", func="", data=None, files=None):
+    async def fetch(self, method="GET", func="", data=None, files=None):
         if data is None:
             data = {}
         try:
@@ -194,10 +190,10 @@ class HQApi(BaseHQApi):
         except json.decoder.JSONDecodeError:
             raise BannedIPError("Your IP is banned")
 
-    def decode_jwt(self, jwt_text: str):
+    async def decode_jwt(self, jwt_text: str):
         return jwt.decode(jwt_text.encode(), verify=False)
 
-    def set_token(self, token):
+    async def set_token(self, token):
         self.token = token
         if self.token:
             self.headers["Authorization"] = "Bearer " + self.token
