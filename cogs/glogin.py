6d5c80f197ed07f0e5e81a4a14471d0e82581a71
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-24 22:09:26 +0530

    update glogin.py

--- cogs/glogin.py
+++ cogs/glogin.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from HQApi import HQApi
-import requests, json
+import aiohttp, json
 from database import db
 import urllib
 from urllib.parse import unquote
@@ -21,15 +21,20 @@ class Google(commands.Cog, HQApi):
         url = 'https://www.googleapis.com/oauth2/v4/token'
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         code = unquote(access_token)
-        data = {"audience":'668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com',
-                   "client_id":'668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com',#'668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com',
-                   "code": code,#"4%2F0AY0e-g7T6nfb3XAPZueeZA3lOo9m03ysOn7pnlbuQsgIgyA86QWzt4bkz1zh94a4J0ZwKQ",#"4%2F0AY0e-g51-ROwgrpCSsZDe8dyIeCT--MmUpbUUFZ2hGpIkMMK0U_76LmzzOeiZJHjvZeH9A",
-                   "grant_type":"authorization_code",
-                   "redirect_uri":"http://localhost:8080",#"com.googleusercontent.apps.668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q:/oauth2callback",
-                   "verifier":'56778634'}#72103384}
-        response = requests.post(url = url, headers = headers, data = data).json()
-        id_token = response.get("id_token")
-        return id_token
+        data = {
+            "audience": '668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com',
+            "client_id": '668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com',
+            "code": code,
+            "grant_type": "authorization_code",
+            "redirect_uri": "http://localhost:8080",
+            "verifier": '56778634'
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url = url, headers = headers, data = data) as response:
+                if response.status != 200: return None
+                response = await response.json()
+                id_token = response.get("id_token")
+                return id_token
         
 
     @commands.command(aliases=["googlelink"])
@@ -59,35 +64,30 @@ class Google(commands.Cog, HQApi):
         if not id_token:
             return await ctx.send(ctx.author.mention + ", **Invalid URL provided!**")
         data = await self.google_login(id_token)
-        return await ctx.send(data)
         id = data["userId"]
         username = data["username"]
         login_token = data["loginToken"]
         access_token = data["accessToken"]
-        check_if_exist = token_base.find_one({"id": user_id,
-                                              "user_id": id})
-        if check_if_exist == None:
-            user_info_dict = {'id': user_id,
-                              'token': access_token,
-                              'username': username, 'user_id': id}
-            token_base.insert_one(user_info_dict)
-            user_info_dict = {'id': user_id,
-                              'login_token': login_token,
-                              'access_token': access_token,
-                              'username': username, 'user_id': id}
-            login_token_base.insert_one(user_info_dict)
-            embed=discord.Embed(title="Account Added ✅", description=f"Successfully add an account with name `{username}`", color=discord.Colour.random())
-            embed.set_thumbnail(url=self.client.user.avatar_url)
-            embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-            await ctx.send(embed=embed)
-            await channel.send(f"{ctx.author} add a account via Google.")
-        else:
+        check_if_exist = db.profile_base.find_one({"id": user_id, "user_id": id})
+        if check_if_exist:
             embed=discord.Embed(title="⚠️ Already Exists", description="This account already exists in bot database. You can't add it again.", color=discord.Colour.random())
             embed.set_thumbnail(url=self.client.user.avatar_url)
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             await ctx.send(embed=embed)
-
-
+        user_info_dict = {
+                            'id': user_id,
+                            'user_id': id,
+                            'access_token': access_token,
+                            'login_token': login_token,
+                            'username': username.lower(),
+                            'auto_play': False
+                        }
+        db.profile_base.insert_one(user_info_dict)
+        embed=discord.Embed(title="Account Added ✅", description=f"Successfully add an account with name `{username}`", color=discord.Colour.random())
+        embed.set_thumbnail(url=self.client.user.avatar_url)
+        embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
+        await ctx.send(embed=embed)
+        await channel.send(f"{ctx.author} add a account via Google.")
 
 def setup(client):
     client.add_cog(Google(client))
