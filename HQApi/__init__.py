999537b9dc0a011084a07089aeaeb5a216b11f75
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-24 18:21:21 +0530

    update __init__.py

--- HQApi/__init__.py
+++ HQApi/__init__.py
@@ -1,5 +1,3 @@
 from HQApi.hq_api import BaseHQApi, HQApi
-from HQApi.hq_websocket import HQWebSocket
-from HQApi.exceptions import ApiResponseError, BannedIPError, WebSocketNotAvailable, NotLive
-
+from HQApi.exceptions import ApiResponseError, BannedIPError
 name = "HQApi"
