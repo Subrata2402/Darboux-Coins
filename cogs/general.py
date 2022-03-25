ac206e8e6e01b9abcf18763876f0b1b792861168
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-26 02:58:45 +0530

    update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -35,7 +35,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        embed=discord.Embed(title="**Invite Me to Your Server !**", description="**Invite Link : [Click Here](https://discord.com/api/oauth2/authorize?client_id=838631852603474001&permissions=523376&scope=bot)**", color=discord.Colour.random())
+        embed=discord.Embed(title="**Invite Me to Your Server !**", description="**Invite Link : [Click Here](https://discord.com/api/oauth2/authorize?client_id={self.client.user.id}&permissions=523376&scope=bot)**", color=discord.Colour.random())
         embed.set_thumbnail(url=self.client.user.avatar_url)
         await ctx.send(embed=embed)
 
