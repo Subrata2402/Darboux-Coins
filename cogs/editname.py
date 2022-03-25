8f4360838914cf2b6edd3bb774b2a1d076080662
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-25 07:42:30 +0530

    update editname.py

--- cogs/editname.py
+++ cogs/editname.py
@@ -1,22 +1,8 @@
 import discord
-import random
 from discord.ext import commands
-import asyncio
 from HQApi import HQApi
 from HQApi.exceptions import ApiResponseError
-from HQApi import HQApi, HQWebSocket
-import asyncio
-from datetime import datetime
-import requests
-import json
-import time
-import colorsys
-import datetime
-import aniso8601
-from pytz import timezone
-from unidecode import unidecode
-from bs4 import BeautifulSoup
-from database.db import token_base, login_token_base
+from database import db
 
 
 class EditUsername(commands.Cog):
@@ -30,23 +16,22 @@ class EditUsername(commands.Cog):
         if not username or not name:
             embed=discord.Embed(title="⚠️ Invalid Command", description=f"Use `{ctx.prefix}editname [username] [new name]` to edit your HQ Trivia account username.", color=discord.Colour.random())
             return await ctx.send(embed=embed)
-        commander_id = ctx.author.id
         name_list = []
-        all_data = list(token_base.find({"id": commander_id, "username": username}))
+        all_data = list(token_base.find())
         for i in all_data:
             name_list.append(i['username'])
-        if username in name_list:
-            token = token_base.find_one({'username': username})['token']
+        check_id = db.profile_base.find_one({"id": ctx.author.id, "username": username.lower()})
+        if check_id:
             try:
-                api = HQApi(token)
-                data = api.get_users_me()
+                api = HQApi(db.profile_base.find_one({"id": ctx.author.id, "username": username.lower()}).get("access_token"))
+                data = await api.get_users_me()
             except ApiResponseError:
                 embed=discord.Embed(title="⚠️ Token Expired", description=f"Your account token is expired. Please refresh your account by this command.\n`{ctx.prefix}refresh {username}`", color=discord.Colour.random())
                 embed.set_thumbnail(url=self.client.user.avatar_url)
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
                 return await ctx.send(embed=embed)
             try:
-                data = api.edit_username(name)
+                data = await api.edit_username(name)
                 embed=discord.Embed(title="**Username Edited Done ✅**", description=f"Successfully Edited username `{username}` to `{name}`", color=discord.Colour.random())
                 embed.set_thumbnail(url=self.client.user.avatar_url)
                 embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
@@ -62,10 +47,6 @@ class EditUsername(commands.Cog):
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             await ctx.send(embed=embed)
 
-    @editname.error
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, Exception):
-            await ctx.send(f'```\n{error}\n```')
 
 def setup(client):
     client.add_cog(EditUsername(client))
