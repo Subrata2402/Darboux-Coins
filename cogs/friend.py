4f368aa3ea973ab37b672636a364ff511ce2b714
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-26 00:18:01 +0530

    update friend.py

--- cogs/friend.py
+++ cogs/friend.py
@@ -1,8 +1,9 @@
-import discord
+import discord, math, datetime
 from discord.ext import commands
 from HQApi import HQApi
 from HQApi.exceptions import ApiResponseError
 from database import db
+rom config.button import peginator_button
 
 class Friends(commands.Cog):
 
@@ -13,6 +14,10 @@ class Friends(commands.Cog):
     @commands.command()
     async def friends(self, ctx, username=None):
         """Get friend lists."""
+        first_page_buttons = await peginator_button(self.client, disabled_1 = True, disabled_2 = True)
+        last_page_buttons = await peginator_button(self.client, disabled_3 = True, disabled_4 = True)
+        middle_page_buttons = await peginator_button(self.client)
+        
         if username is None:
             embed=discord.Embed(title="⚠️ Invalid Command", description=f"Use `{ctx.prefix}friends [username]` to check your all friends list.", color=discord.Colour.random())
             return await ctx.send(embed=embed)
@@ -22,8 +27,7 @@ class Friends(commands.Cog):
             embed.set_thumbnail(url=self.client.user.avatar_url)
             embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
             return await ctx.send(embed=embed)
-        if ctx.guild:
-            await ctx.send(f"{ctx.author.mention}, **Check your DM!**")
+        if ctx.guild: await ctx.send(f"{ctx.author.mention}, **Check your DM!**")
         try:
             api = HQApi(db.profile_base.find_one({"id": ctx.author.id, "username": username.lower()}).get("access_token"))
             data = await api.get_users_me()
@@ -34,45 +38,73 @@ class Friends(commands.Cog):
             return await ctx.author.send(embed=embed)
         username = data["username"]
         response = await api.friend_list()
-        embed1=discord.Embed(title=f"**__{username}'s Friends List !__**", color=discord.Colour.random())
-        embed2=discord.Embed(color=discord.Colour.random())
-        embed3=discord.Embed(color=discord.Colour.random())
         embed=discord.Embed(title=f"Loading {username}'s friends list...", color=discord.Colour.random())
-        x = await ctx.author.send(embed=embed)
-        for index, data in enumerate(response["data"]):
+        message = await ctx.author.send(embed=embed)
+        embed=discord.Embed(title=f"**__{username}'s Friends List !__**", color=discord.Colour.random())
+        friends_data = response["data"]
+        page = 1
+        items_per_page = 10
+        pages = math.ceil(len(friends_data) / items_per_page)
+        start = (page - 1) * items_per_page
+        end = start + items_per_page
+        for index, data in enumerate(friends_data[start:end], start=start):
             name = data["username"]
             total = data["leaderboard"]["total"]
             highScore = data["highScore"]
             gamesPlayed = data["gamesPlayed"]
             winCount = data["winCount"]
-            name = f"{index+1}. {name}"
-            value = f"> Total Winnings : {total}\n> High Score : {highScore}\n> Games Won : {winCount}/{gamesPlayed}"
-            s = index + 1
-            if s < 21:
-                embed1.add_field(name=name, value=value)
-            elif s < 41:
-                embed2.add_field(name=name, value=value)
+            name = f"{'0' if index+1 < 10 else ''}{index+1}. {name}"
+            value = f"Total Winnings : {total}\nHigh Score : {highScore}\nGames Won : {winCount}/{gamesPlayed}"
+            embed.add_field(name=name, value=value, inline = False)
+        pages = 1 if pages == 0 else pages
+        embed.set_footer(text=f"Page : {'0' if page < 10 else ''}{page}/{'0' if pages < 10 else ''}{pages}")
+        embed.timestamp = datetime.datetime.utcnow()
+        if pages in [0, 1]:
+            return await message.edit(embed = embed)
+        else:
+            if page == 1:
+                await message.edit(embed = embed, components=first_page_buttons)
+                embed.clear_fields()
+        def check(interaction):
+            return interaction.message == message
+        while True:
+            try:
+                interaction = await self.client.wait_for('button_click', check=check, timeout=60.0)
+            except:
+                buttons = await peginator_button(client = self.client, disabled_1 = True, disabled_2 = True, disabled_3 = True, disabled_4 = True)
+                return await message.edit(components = buttons)
+            
+            if interaction.custom_id == "button4":
+                page = pages
+            elif interaction.custom_id == "button3":
+                page += 1
+                if page > pages: page = pages
+            elif interaction.custom_id == "button2":
+                page -= 1
+                if page <= 0: page = 1
             else:
-                embed3.add_field(name=name, value=value)
-        if s == 0:
-            embed=discord.Embed(title=f"**__{username}'s Friends List !__**", description=f"Couldn't find any friends in your friend list.", color=discord.Colour.random())
-            embed.set_thumbnail(url=self.client.user.avatar_url)
-            embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-            return await ctx.author.send(embed=embed)
-        if s > 0:
-            embed1.set_thumbnail(url=self.client.user.avatar_url)
-            embed1.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-            await x.edit(embed=embed1)
-        if s > 20:
-            embed2.set_thumbnail(url=self.client.user.avatar_url)
-            embed2.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-            await ctx.author.send(embed=embed2)
-        if s > 40:
-            embed3.set_thumbnail(url=self.client.user.avatar_url)
-            embed3.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-            await ctx.author.send(embed=embed3)
-
-        
+                page = 1
+                
+            start = (page - 1) * items_per_page
+            end = start + items_per_page
+            for index, data in enumerate(friends_data[start:end], start=start):
+                name = data["username"]
+                total = data["leaderboard"]["total"]
+                highScore = data["highScore"]
+                gamesPlayed = data["gamesPlayed"]
+                winCount = data["winCount"]
+                name = f"{'0' if index+1 < 10 else ''}{index+1}. {name}"
+                value = f"Total Winnings : {total}\nHigh Score : {highScore}\nGames Won : {winCount}/{gamesPlayed}"
+                embed.add_field(name=name, value=value, inline = False)
+            embed.set_footer(text=f"Page : {'0' if page < 10 else ''}{page}/{'0' if pages < 10 else ''}{pages}")
+            embed.timestamp = datetime.datetime.utcnow()
+            if page == 1:
+                await interaction.respond(type = 7, embed=embed, components=first_page_buttons)
+            elif page == pages:
+                await interaction.respond(type = 7, embed=embed, components=last_page_buttons)
+            else:
+                await interaction.respond(type = 7, embed=embed, components=middle_page_buttons)
+            embed.clear_fields()
 
     @commands.command()
     async def addfriend(self, ctx, username=None, name=None):
