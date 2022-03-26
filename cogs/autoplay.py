244f2ecc48968cee1ee325d07f375a5857a63687
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-26 15:14:45 +0530

    update autoplay.py

--- cogs/autoplay.py
+++ cogs/autoplay.py
@@ -68,9 +68,9 @@ class AutoPlay(commands.Cog):
                         tcoins = str(data['gameSummary']['coinsTotal'])
                         coins = str(data['gameSummary']['coinsEarned'])
                         correct = str(data['gameSummary']['questionsCorrect'])
-                        embed=discord.Embed(title="Playing HQ Offair Trivia...", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct}/12\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
+                        embed=discord.Embed(title="Played HQ Offair Trivia ✅", description=f"**• Username : {username}\n• Games Played : 01\n• Questions Correct : {correct}/12\n• Coins Earned : {coins}\n• Total Coins : {tcoins}**", color=discord.Colour.random())
                         embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
-                        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/773955381063974972.gif")
+                        embed.set_thumbnail(url=self.client.user.avatar_url)
                         channel = self.client.get_channel(957198388028375050)
                         await channel.send(embed = embed)
                         break
