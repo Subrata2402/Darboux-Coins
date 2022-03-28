248ab619b7f7f4ca268ac25e2b63befe4196b383
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-28 23:29:23 +0530

    update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -1,1 +1,118 @@
+import math
+import os
+import sys
+import traceback
+from utils.emoji import cross, warning
+import discord
+from discord.ext import commands
 
+class Errors(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        channel = self.client.get_channel(958059478538936400)
+        embed = discord.Embed(description = f"Command : `{ctx.command.name}`\nGuild : `{ctx.guild.name}`\nChannel : `{ctx.channel.name}`\nCommand Failed : `{ctx.command_failed}`\nMessage :\n```\n{ctx.message.content}\n```",
+                color = discord.Color.random(),
+                timestamp = ctx.author.created_at)
+        embed.set_footer(text = f"ID : {ctx.author.id} | Created at")
+        embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+        embed.set_thumbnail(url = ctx.guild.icon_url)
+        await channel.send(embed = embed)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Error cog loaded successfully")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        # get the original exception
+        error = getattr(error, "original", error)
+
+        if isinstance(error, commands.CommandOnCooldown):
+            retry_after = int(error.retry_after)
+            minutes, seconds = divmod(retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            if hours + minutes == 0:
+                wait_time = f"**{'0' if seconds < 10 else ''}{seconds}** second{'s' if seconds != 1 else ''}"
+            elif hours == 0:
+                wait_time = f"**{'0' if minutes < 10 else ''}{minutes}** minute{'s' if minutes != 1 else ''} **{'0' if seconds < 10 else ''}{seconds}** second{'s' if seconds != 1 else ''}"
+            elif minutes == 0:
+                wait_time = f"**{'0' if hours < 10 else ''}{hours}** hour{'s' if hours != 1 else ''} **{'0' if seconds < 10 else ''}{seconds}** second{'s' if seconds != 1 else ''}"
+            else:
+                wait_time = f"**{'0' if hours < 10 else ''}{hours}** hour{'s' if hours != 1 else ''} **{'0' if minutes < 10 else ''}{minutes}** minute{'s' if minutes != 1 else ''} and **{'0' if seconds < 10 else ''}{seconds}** second{'s' if seconds != 1 else ''}"
+            embed = discord.Embed(
+                title=f"{warning} | Command on Cooldown",
+                description=f"This command is on cooldown, please retry after {wait_time}.",
+                color=discord.Colour.random(),
+            )
+            return await ctx.send(embed=embed)
+            
+        if isinstance(error, commands.TooManyArguments):
+            embed = discord.Embed(
+                description=f"{warning} | You given one or more extra arguments which are not required!",
+                color=discord.Colour.random(),
+            )
+            return await ctx.send(embed=embed)
+
+
+        if isinstance(error, commands.MaxConcurrencyReached):
+            if error.per == commands.BucketType.guild:
+                if error.number == 1:
+                    embed=discord.Embed(description=f"{cross} | This command is already running in this server. Please wait for it to finish.", color=discord.Colour.random())
+                    return await ctx.send(embed=embed)
+                embed=discord.Embed(description=f"{cross} | {error.number} commands is already running in this server. Please wait for it to finish.", color=discord.Colour.random())
+                return await ctx.send(embed=embed)
+            else:
+                embed=discord.Embed(description=f"{cross} | You've already started a game, finish it first and then try another.", color=discord.Colour.random())
+                return await ctx.send(embed=embed)
+                
+
+        if isinstance(error, commands.NotOwner):
+            embed = discord.Embed(color=discord.Colour.random())
+            embed.description = f"{cross} | This command only for bot developers. You can't use it."
+            return await ctx.send(embed=embed)
+
+
+        if isinstance(error, discord.errors.Forbidden):
+            try:
+                embed = discord.Embed(
+                    title=f"{warning} | Forbidden Error",
+                    description=f"Error - 404 | Missing Permission(s)",
+                    color=discord.Colour.random(),
+                )
+                await ctx.send(embed=embed)
+            except discord.Forbidden:
+                try:
+                    embed = discord.Embed(description = f"Hey it looks like I can't send messages in {ctx.channel.mention} channel.")
+                    await ctx.author.send(content = ctx.author.mention, embed = embed)
+                except:
+                    pass
+            return
+
+        if isinstance(error, commands.UserInputError):
+            return await ctx.send(ctx.author.mention + " You provided an invalid argument. Please check the command usage and try again.")
+
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        channel = await self.client.fetch_channel(958059379041640448)
+        embed=discord.Embed(title=f"{warning} | Found an error", description=f"Ignoring exception in command : `{ctx.command}`", color=discord.Colour.random())
+        embed.add_field(name = "Error Detected :", value=f"```\n{error}\n```")
+        embed.add_field(name = "Cog Name :", value = ctx.command.cog_name)
+        try:
+            await channel.send(embed=embed)
+        except:
+            pass
+        print(f"Ignoring exception in command {ctx.command}: Cog Name : {ctx.command.cog_name}", file=sys.stderr)
+        traceback.print_exception(
+            type(error), error, error.__traceback__, file=sys.stderr
+            )
+
+
+def setup(client):
+    client.add_cog(Errors(client))
