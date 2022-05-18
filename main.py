da3b3c513508cce186bbe29a45e7f0a9d678fa16
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-18 16:30:21 +0530

    update main.py

--- main.py
+++ main.py
@@ -84,12 +84,13 @@ async def on_message(message):
 	if message.author.id == 433615162394804224 and message.channel.id == 844547681838039041:
 		for embed in message.embeds:
 			to_dict = embed.to_dict()
-			print(to_dict)
-			embed.description = to_dict["description"] + f"\n\n**__Source :__** [Click Here]"
+			embed.description = to_dict["description"] + f"\n**__Source :__** [Click Here]({to_dict["url"]})"
 			embed.color = discord.Colour.random()
-			embed.set_footer(text = "HQ Tweets")
+			embed.set_footer(text = "HQ Tweets", icon_url = "")
+			embed.set_thumbnail(url = "")
+			embed.set_author.icon_url = to_dict["thumbnail"]["url"]
 			embed.timestamp = datetime.datetime.utcnow()
-#await client.get_channel(976420002020343838).send(content = "<@&844552999983513650>", embed = embed)
+			await client.get_channel(976420002020343838).send(content = "<@&844552999983513650>", embed = embed)
 	if message.content.startswith(client.user.mention):
 		await message.channel.send(f"Hey {message.author.mention}, My prefix is `-` For more information use `-help`")
 	await client.process_commands(message)
