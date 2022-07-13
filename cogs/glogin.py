d2db4318d8c8f4bd112890236b2a8f233affc812
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-07-13 07:54:06 +0530

    Update glogin.py

--- cogs/glogin.py
+++ cogs/glogin.py
@@ -40,7 +40,7 @@ class Google(commands.Cog, HQApi):
     async def glink(self, ctx):
         if ctx.guild:
             await ctx.send(f"{ctx.author.mention}, **Check your DM!**")
-        with open("Video/VID_20220426041555.mp4", "rb") as f:
+        with open("Video/VID-20220713-WA0000.mp4", "rb") as f:
             file = discord.File(f, filename = "add-account-with-google.mp4", spoiler = False)
             embed=discord.Embed(title="**HQ Google Login**", description=f"**[Click Here](https://accounts.google.com/o/oauth2/v2/auth?audience=668326540387-84isqp5u1s4dubes1tns5i7p2kgqefja.apps.googleusercontent.com&client_id=668326540387-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=http://localhost:8080&verifier=56778634) to login HQ Trivia with Google Account.\n\nUse `{ctx.prefix}gmethod` to get all process of Google Login.**", color=discord.Colour.random())
             #embed.add_field(name="Login Link", value="[Click Here](87-isfa1c5ibd6h0mhm2h10n242q2uc131q.apps.googleusercontent.com&response_type=code&scope=email%20profile&&redirect_uri=https://localhost:8000&verifier=56778634)")
