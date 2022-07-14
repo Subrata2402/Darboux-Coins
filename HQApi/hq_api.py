4f4b01a60dff678406c8cc9d58e2e5f989396533
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-07-14 22:09:25 +0530

    update hq_api.py

--- HQApi/hq_api.py
+++ HQApi/hq_api.py
@@ -394,7 +394,9 @@ class HQApi(BaseHQApi):
         self.v = verify
         self.p = dict(http=proxy, https=proxy)
         self.headers = {
-            "x-hq-client": "Android/" + self.hq_version}
+            # "x-hq-client": "Android/" + self.hq_version
+            "x-hq-client": "iPhone8,2"
+        }
         if login_token:
             self.token = asyncio.run(self.get_tokens(login_token))["accessToken"]
         if self.token:
