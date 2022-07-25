a218b60e64fefe34bed231824a020609ffd07723
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-07-25 15:07:51 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -4,3 +4,7 @@ data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb
 db = data.get_database("Darboux")
 profile_base = db.profile
 questions_base = db.hq_questions
+
+client = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/MimirQuiz?retryWrites=true&w=majority')
+dbase = client.get_database("MimirQuiz")
+sb_details = dbase.sb_details
