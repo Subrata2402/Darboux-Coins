8c7489f8a3cabf449a8c9059aab753607f9a75a6
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-08-05 09:36:40 +0530

    update self_roles.py

--- cogs/self_roles.py
+++ cogs/self_roles.py
@@ -10,7 +10,7 @@ class SelfRoles(commands.Cog):
         self.custom_ids = ["cashout_updates", "hq_tweets", "announcement", "bot_updates"]
 
     @commands.Cog.listener()
-    async def on_button_click(self, interaction: Interaction):
+    async def on_select_option(self, interaction: Interaction):
         if interaction.responded: return
         if interaction.custom_id not in self.custom_ids: return
         embed = discord.Embed(color=discord.Colour.random())
@@ -24,4 +24,14 @@ class SelfRoles(commands.Cog):
     async def _self_role(self, ctx):
         embed = discord.Embed(title = "Notification Roles", color=discord.Colour.random())
         embed.description = "Select from the following notification roles to be alerted when certain things happen in our community!"
-        embed.set_image(url = )
+        embed.set_image(url = "https://im.ge/i/FbW2A9")
+        components = [
+                Select(
+                        placeholder = "Select a role...",
+                        options = [
+                                SelectOption(label = "Bot Updates", description = "DarbouxCoins bot updates", emoji = "🤖", value = "bot_updates"),
+                                SelectOption(label = "Cashout Updates", description = "HQ Trivia cashout & payouts Updates", emoji = "💰", value = "cashout_updates"),
+                            ],
+                        custom_id = "self_roles",
+                    ),
+        await ctx.send(embed = embed, components = components)
