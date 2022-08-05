03b980c2cb108c59a7ec369d1ef1a01dba9fe6fe
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-08-05 11:20:18 +0530

    update self_roles.py

--- cogs/self_roles.py
+++ cogs/self_roles.py
@@ -21,7 +21,7 @@ class SelfRoles(commands.Cog):
         if component_label in [role.name for role in interaction.author.roles]:
             await interaction.author.remove_roles(discord.utils.get(interaction.guild.roles, name = component_label))
             return await interaction.send(f"You've been removed from the ```{component_label}``` role.")
-        await interaction.author.remove_roles(discord.utils.get(interaction.guild.roles, name = component_label))
+        await interaction.author.add_roles(discord.utils.get(interaction.guild.roles, name = component_label))
         await interaction.send(f"You've been added to the ```{component_label}``` role.")
     
     @commands.command(name = "selfrole")
