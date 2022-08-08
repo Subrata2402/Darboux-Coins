0fbe91add1cebb9e51b46ff703cbe1fdd39074e5
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-08-08 12:35:03 +0530

    update button.py

--- config/button.py
+++ config/button.py
@@ -33,12 +33,31 @@ async def hqshop_button(client, disabled_1 = None,
                                 disabled_7 = None,
                                 disabled_8 = None,
                                 disabled_9 = None):
-    life_emoji_1 = client.get_emoji()
-    life_emoji_3 = client.get_emoji()
-    life_emoji_5 = client.get_emoji()
-    eraser_emoji_1 = client.get_emoji()
-    eraser_emoji_3 = client.get_emoji()
-    eraser_emoji_5 = client.get_emoji()
-    spin_emoji_1 = client.get_emoji()
-    spin_emoji_3 = client.get_emoji()
-    spin_emoji_5 = client.get_emoji()
+    life_emoji_1 = client.get_emoji(1006086382147604490)
+    life_emoji_3 = client.get_emoji(1006086589425909870)
+    life_emoji_5 = client.get_emoji(1006086677170753576)
+    eraser_emoji_1 = client.get_emoji(1006087038384222330)
+    eraser_emoji_3 = client.get_emoji(1006087104440303646)
+    eraser_emoji_5 = client.get_emoji(1006087158437793874)
+    spin_emoji_1 = client.get_emoji(1006087362578763886)
+    spin_emoji_3 = client.get_emoji(1006087466870132836)
+    spin_emoji_5 = client.get_emoji(1006087510163734589)
+    
+    button = [
+        [
+            Button(style = ButtonStyle.blue, emoji = life_emoji_1, disabled = disabled_1, custom_id = "life_1"),
+            Button(style = ButtonStyle.blue, emoji = life_emoji_3, disabled = disabled_2, custom_id = "life_3"),
+            Button(style = ButtonStyle.blue, emoji = life_emoji_5, disabled = disabled_3, custom_id = "life_5")
+        ],
+        [
+            Button(style = ButtonStyle.blue, emoji = eraser_emoji_1, disabled = disabled_4, custom_id = "eraser_1"),
+            Button(style = ButtonStyle.blue, emoji = eraser_emoji_3, disabled = disabled_5, custom_id = "eraser_3"),
+            Button(style = ButtonStyle.blue, emoji = eraser_emoji_5, disabled = disabled_6, custom_id = "eraser_5")
+        ],
+        [
+            Button(style = ButtonStyle.blue, emoji = spin_emoji_1, disabled = disabled_7, custom_id = "spin_1"),
+            Button(style = ButtonStyle.blue, emoji = spin_emoji_3, disabled = disabled_8, custom_id = "spin_3"),
+            Button(style = ButtonStyle.blue, emoji = spin_emoji_5, disabled = disabled_9, custom_id = "spin_5")
+        ]
+    ]
+    return button
