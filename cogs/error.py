8d70ef8e9ddc640aadc7db46b96b6a4926fc0163
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-08-11 11:11:00 +0530

    update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -11,14 +11,6 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        guild = self.client.get_guild(831051146880614431)
-        if guild not in ctx.author.mutual_guilds:
-            embed = discord.Embed(title = "❎ Not Found", color = discord.Colour.random(),
-                description = "You need to join our official discord server to use the bot. [Click Here](https://discord.gg/TAcEnfS8Rs) to join the server.")
-            embed.set_thumbnail(url = self.client.user.avatar_url)
-            embed.set_footer(text = ctx.author, icon_url = ctx.author.avatar_url)
-            #await ctx.send(embed = embed)
-            raise Exception("User not in the official server ")
         channel = self.client.get_channel(958059478538936400)
         embed = discord.Embed(description = f"Command : `{ctx.command.name}`\nGuild : `{ctx.guild.name if ctx.guild else None}`\nChannel : `{ctx.channel.name if ctx.guild else ctx.channel}`\nCommand Failed : `{ctx.command_failed}`\nMessage :\n```\n{ctx.message.content}\n```",
                 color = discord.Color.random(),
