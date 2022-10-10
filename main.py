3b552fedf92815d339560b038214ed9bab58ad1d
Subrata2402 <subratadas3250@gmail.com>
2022-10-10 08:40:22 +0530

    update all files

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-import discord, asyncio
+import discord
+import asyncio
 from discord.ext import commands
-import time, datetime
-import sys, traceback, os
-from discord_components import *
+import time
+import datetime
+import sys
+import traceback
+discord.utils.setup_logging()
 
-class DarbouxCoins(commands.Cog):
-    
+
+initial_extensions = [
+        # "Cogs.general",
+    ]
+
+
+class DarbouxCoins(commands.Bot):
+
     def __init__(self, client):
         self.client = client
-        
+
     @commands.Cog.listener()
     async def on_ready(self):
         print('====================')
         print(self.client.user)
         channel = self.client.get_channel(835743589241454592)
-        embed=discord.Embed(title="Bot Updated ✅", description="Bot successfully updated. No issues found!", color=discord.Colour.random())
+        embed = discord.Embed(
+            title="Bot Updated ✅", description="Bot successfully updated. No issues found!", color=discord.Colour.random())
         embed.set_thumbnail(url=self.client.user.avatar_url)
-        embed.set_footer(text=self.client.user, icon_url=self.client.user.avatar_url)
+        embed.set_footer(text=self.client.user,
+                         icon_url=self.client.user.avatar_url)
         await channel.send(embed=embed)
         while True:
-            #await self.client.change_presence(activity=discord.Activity(type=3,name="on "+str(len(self.client.guilds))+" servers | -invite"))
-            #await asyncio.sleep(5)
+            # await self.client.change_presence(activity=discord.Activity(type=3,name="on "+str(len(self.client.guilds))+" servers | -invite"))
+            # await asyncio.sleep(5)
             await self.client.change_presence(status=discord.Status.online, activity=discord.Game(name="with -dcplay <username>", type=2))
             await asyncio.sleep(5)
             await self.client.change_presence(status=discord.Status.online, activity=discord.Game(name="with Help ➜ -help", type=2))
@@ -31,38 +42,39 @@ class DarbouxCoins(commands.Cog):
             await asyncio.sleep(5)
 
     @commands.command(
-        name = "say",
-        description = "Send message by bot.",
-        aliases = ["tell", "echo"],
-        usage = "[message]",
-        brief = "Hello, what's up!"
-        )
+        name="say",
+        description="Send message by bot.",
+        aliases=["tell", "echo"],
+        usage="[message]",
+        brief="Hello, what's up!"
+    )
     @commands.is_owner()
     async def say(self, ctx, *, message):
         await ctx.message.delete()
         await ctx.send(message)
-        
+
     @commands.command(
-        name = "sayembed",
-        description = "Send message with embed.",
-        aliases = [],
-        usage = "",
-        brief = ""
-        )
+        name="sayembed",
+        description="Send message with embed.",
+        aliases=[],
+        usage="",
+        brief=""
+    )
     @commands.is_owner()
     async def sayem(self, ctx, *, message):
         """Send Message With Embed."""
         await ctx.message.delete()
-        embed=discord.Embed(description=message, color=discord.Colour.random())
+        embed = discord.Embed(description=message,
+                              color=discord.Colour.random())
         await ctx.send(embed=embed)
-    
+
     @commands.command(
-        name = "ping",
-        description = "Get bot latency.",
-        aliases = ["pong"],
-        usage = "",
-        brief = ""
-        )
+        name="ping",
+        description="Get bot latency.",
+        aliases=["pong"],
+        usage="",
+        brief=""
+    )
     async def ping(self, ctx):
         """ Pong! """
         before = time.monotonic()
@@ -70,47 +82,55 @@ class DarbouxCoins(commands.Cog):
         ping = (time.monotonic() - before) * 1000
         await message.edit(content=f"**__Pong!__** :ping_pong:  **{int(ping)}ms**")
 
+
 bot_prefix = "-", "+", "."
 intents = discord.Intents.all()
 ids = [790190926630486016, 660337342032248832]
-client = commands.Bot(command_prefix = bot_prefix, intents=intents, strip_after_prefix=True, owner_ids = ids, case_insensitive = True)
+client = commands.Bot(command_prefix=bot_prefix, intents=intents,
+                      strip_after_prefix=True, owner_ids=ids, case_insensitive=True)
 client.remove_command('help')
-DiscordComponents(client)
 client.add_cog(DarbouxCoins(client))
+
 
 @client.event
 async def on_message(message):
-    cmd = client.get_command(message.content[1:].strip().lower().partition(' ')[0])
+    cmd = client.get_command(
+        message.content[1:].strip().lower().partition(' ')[0])
     if cmd:
         guild = client.get_guild(831051146880614431)
         if not message.author.bot and guild not in message.author.mutual_guilds:
-            embed = discord.Embed(title = "❎ Not Found", color = discord.Colour.random(),
-                description = "You need to join our official discord server to use the bot. [Click Here](https://discord.gg/TAcEnfS8Rs) to join the server.")
-            embed.set_thumbnail(url = client.user.avatar_url)
-            embed.set_footer(text = message.author, icon_url = message.author.avatar_url)
-            return await message.channel.send(embed = embed)
-    if message.channel.id == 831051147472666636: return
+            embed = discord.Embed(title="❎ Not Found", color=discord.Colour.random(),
+                                  description="You need to join our official discord server to use the bot. [Click Here](https://discord.gg/TAcEnfS8Rs) to join the server.")
+            embed.set_thumbnail(url=client.user.avatar_url)
+            embed.set_footer(text=message.author,
+                             icon_url=message.author.avatar_url)
+            return await message.channel.send(embed=embed)
+    if message.channel.id == 831051147472666636:
+        return
     if message.author.id == 433615162394804224 and message.channel.id == 844547681838039041:
         for embed in message.embeds:
             to_dict = embed.to_dict()
-            embed.description = to_dict["description"] + f"\n**Source :** [Click Here]({to_dict['url']})"
+            embed.description = to_dict["description"] + \
+                f"\n**Source :** [Click Here]({to_dict['url']})"
             embed.color = discord.Colour.random()
-            embed.set_footer(text = "HQ Tweets", icon_url = "https://media.discordapp.net/attachments/827262575439380542/976439492644843610/625974899051069460.png")
-            embed.set_thumbnail(url = "")
-            embed.set_author(icon_url = to_dict["thumbnail"]["url"], name = to_dict["author"]["name"])
+            embed.set_footer(
+                text="HQ Tweets", icon_url="https://media.discordapp.net/attachments/827262575439380542/976439492644843610/625974899051069460.png")
+            embed.set_thumbnail(url="")
+            embed.set_author(
+                icon_url=to_dict["thumbnail"]["url"], name=to_dict["author"]["name"])
             embed.timestamp = datetime.datetime.utcnow()
-            await client.get_channel(976420002020343838).send(embed = embed, content = "<@&1005187248284778567>")
+            await client.get_channel(976420002020343838).send(embed=embed, content="<@&1005187248284778567>")
     if message.content.startswith(client.user.mention):
         await message.channel.send(f"Hey {message.author.mention}, My prefix is `-` For more information use `-help`")
     await client.process_commands(message)
 
 extensions = [
-        "login", "show", "hqname", "welcome", "swipe", "google_login_method",
-        "logintoken", "token", "payout", "dcplay", "balance", "self_roles",
-        "editname", "userinfo", "details", "help", "general", "test",
-        "rwin", "refresh", "friend", "sdcplay", "items", "error",
-        "fblogin", "glogin", "profile", "fb_login_method", "autoplay"
-    ]
+    "login", "show", "hqname", "welcome", "swipe", "google_login_method",
+    "logintoken", "token", "payout", "dcplay", "balance", "self_roles",
+    "editname", "userinfo", "details", "help", "general", "test",
+    "rwin", "refresh", "friend", "sdcplay", "items", "error",
+    "fblogin", "glogin", "profile", "fb_login_method", "autoplay"
+]
 
 if __name__ == "__main__":
     for extension in extensions:
